c56e7990588379b92bbe94dba4108592458a47d7
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-03-19 18:18:55 -0300

    icones e barras funcionando

--- GUI.java
+++ GUI.java
@@ -1,53 +1,124 @@
 import javax.swing.*;
 import java.awt.*;
 
+// GRAPHIC USER INTERFACE
 public class GUI {
 
     public static void main(String[] args) {
-        Tamagotchi teste = new Tamagotchi("Zé");
+        Tamagotchi tamagotchi = new Tamagotchi("Zé");
 
-        ImageIcon icon = new ImageIcon("./fome.png");
-
-        JButton button = new JButton(icon);
-        button.setBorder(BorderFactory.createEmptyBorder());
-        button.setContentAreaFilled(false);
-
-        button.addActionListener(e -> {
+        // Icone Botao Fome
+        ImageIcon iconeFome = new ImageIcon("./fome2.png");
+        JButton botaoFome = new JButton(iconeFome);
+        botaoFome.setBorder(BorderFactory.createEmptyBorder());
+        botaoFome.setContentAreaFilled(false);
+        botaoFome.addActionListener(e -> {
             // Handle button click here
-            System.out.println("Button clicked!");
+            tamagotchi.comer();
         });
 
-        JFrame frame = new JFrame("Image Button Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.getContentPane().add(button, BorderLayout.PAGE_START);
-        //frame.pack();
-        //frame.setVisible(true);
-        
-        // Crie um JFrame
-        //JFrame frame2 = new JFrame("Exemplo de Barra de Progresso Horizontal");
-        //frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame2.setSize(300, 100);
+        // Icone Botao Energia
+        ImageIcon iconeEnergia = new ImageIcon("./energia2.png");    
+        JButton botaoEnergia = new JButton(iconeEnergia);
+        botaoEnergia.setBorder(BorderFactory.createEmptyBorder());
+        botaoEnergia.setContentAreaFilled(false);
+        botaoEnergia.addActionListener(e -> {
+            // Handle button click here
+            tamagotchi.dormir();
+        });
 
-        // Crie uma JProgressBar
+        // Icone Botao Felicidade
+        ImageIcon iconeFelicidade = new ImageIcon("./dar-felicidade.png");    
+        JButton botaoFelicidade = new JButton(iconeFelicidade);
+        botaoFelicidade.setBorder(BorderFactory.createEmptyBorder());
+        botaoFelicidade.setContentAreaFilled(false);
+        botaoFelicidade.addActionListener(e -> {
+            // Handle button click here
+            tamagotchi.brincar();
+        });
+
+        // Icone Botao Saude
+        ImageIcon iconeSaude = new ImageIcon("./cuidados-de-saude.png");    
+        JButton botaoSaude = new JButton(iconeSaude);
+        botaoSaude.setBorder(BorderFactory.createEmptyBorder());
+        botaoSaude.setContentAreaFilled(false);
+        botaoSaude.addActionListener(e -> {
+            // Handle button click here
+            tamagotchi.cuidar();
+        });
+
+        // Criando o frame e adicionando os botões
+        JFrame frame = new JFrame("Java Tamagotchi");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints restricoes = new GridBagConstraints();
+        restricoes.fill = GridBagConstraints.HORIZONTAL;
+        //restricoes.gridwidth = 2;
+        restricoes.gridx = 0;
+        restricoes.gridy = 0;
+        restricoes.insets = new Insets(0, 50, 0, 50);
+        frame.add(botaoEnergia, restricoes);
+        restricoes.gridx = 1;
+        frame.add(botaoFome, restricoes);
+        restricoes.gridx = 2;
+        frame.add(botaoFelicidade, restricoes);
+        restricoes.gridx = 3;
+        frame.add(botaoSaude, restricoes);
+
+        // Barra de Fome
         JProgressBar barraDeFome = new JProgressBar(JProgressBar.HORIZONTAL);
         barraDeFome.setMinimum(0);
         barraDeFome.setMaximum(10);
-        barraDeFome.setValue(teste.getFome()); // Defina um valor inicial (por exemplo, 50%)
+        barraDeFome.setValue(10); // Defina um valor inicial (por exemplo, 100%)
         barraDeFome.setForeground(Color.GREEN);
-        // Adicione a barra de progresso ao frame
-        frame.add(barraDeFome, BorderLayout.CENTER);
+        restricoes.gridx = 1;
+        restricoes.gridy = 1;
+        frame.add(barraDeFome, restricoes);
 
+        // Barra de Energia
+        JProgressBar barraDeEnergia = new JProgressBar(JProgressBar.HORIZONTAL);
+        barraDeEnergia.setMinimum(0);
+        barraDeEnergia.setMaximum(10);
+        barraDeEnergia.setValue(10); // Defina um valor inicial (por exemplo, 100%)
+        barraDeEnergia.setForeground(Color.GREEN);
+        restricoes.gridx = 0;
+        restricoes.gridy = 1;
+        frame.add(barraDeEnergia, restricoes);
+
+        // Barra de Felicidade
+        JProgressBar barraDeFelicidade = new JProgressBar(JProgressBar.HORIZONTAL);
+        barraDeFelicidade.setMinimum(0);
+        barraDeFelicidade.setMaximum(10);
+        barraDeFelicidade.setValue(10); // Defina um valor inicial (por exemplo, 100%)
+        barraDeFelicidade.setForeground(Color.GREEN);
+        restricoes.gridx = 2;
+        restricoes.gridy = 1;
+        frame.add(barraDeFelicidade, restricoes);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Define o estado da janela como maximizado
+
+        // Barra de Saude
+        JProgressBar barraDeSaude = new JProgressBar(JProgressBar.HORIZONTAL);
+        barraDeSaude.setMinimum(0);
+        barraDeSaude.setMaximum(10);
+        barraDeSaude.setValue(10); // Defina um valor inicial (por exemplo, 100%)
+        barraDeSaude.setForeground(Color.GREEN);
+        restricoes.gridx = 3;
+        restricoes.gridy = 1;
+        frame.add(barraDeSaude, restricoes);
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Define o estado da janela como maximizado
         // Exiba o frame
-        //frame2.setVisible(true);
-        tornarJanelaTelaCheia(frame);
+        frame.setVisible(true);
 
         while (true){
-            atualizaBarraFome(barraDeFome, teste);
+            atualizaNivelBarra(barraDeEnergia, tamagotchi.getEnergia());
+            atualizaNivelBarra(barraDeFome, tamagotchi.getFome());
+            atualizaNivelBarra(barraDeSaude, tamagotchi.getSaude());
+            atualizaNivelBarra(barraDeFelicidade, tamagotchi.getFelicidade());
             
         }
     }
-
+/* 
     private static void tornarJanelaTelaCheia(JFrame janela) {
         GraphicsDevice dispositivo = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         janela.setUndecorated(true); // Remove a barra de título e bordas
@@ -55,19 +126,19 @@ public class GUI {
         dispositivo.setFullScreenWindow(janela); // Coloca a janela em modo de tela cheia
         janela.setVisible(true); // Torna a janela visível
     }
-    private static void atualizaBarraFome(JProgressBar barraDeFome, Tamagotchi tamagotchi){
-        int nivelFome = tamagotchi.getFome();
-        if (nivelFome >= 6){
-            barraDeFome.setValue(tamagotchi.getFome()); 
-            barraDeFome.setForeground(Color.GREEN);
+*/
+    private static void atualizaNivelBarra(JProgressBar barra, int valorAtual){
+        if (valorAtual >= 6){
+            barra.setValue(valorAtual); 
+            barra.setForeground(Color.GREEN);
         } 
-        else if (nivelFome > 3){
-            barraDeFome.setValue(tamagotchi.getFome()); 
-            barraDeFome.setForeground(Color.YELLOW); 
+        else if (valorAtual > 3){
+            barra.setValue(valorAtual); 
+            barra.setForeground(Color.YELLOW); 
         } 
         else {
-            barraDeFome.setValue(tamagotchi.getFome()); 
-            barraDeFome.setForeground(Color.RED);
+            barra.setValue(valorAtual); 
+            barra.setForeground(Color.RED);
         }
     }
 
